159c8b2ac02fe82418c44a543dec49906e6774f9
Michael Seifi <michael.seifi@fairwaymc.com>
2021-06-21 14:35:19 -0500

    initialize

---
+++ Code/SendGrid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendGrid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
