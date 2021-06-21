f532a0ffd7bb386ef662f77957f482cfefa98ce4
Michael Seifi <michael.seifi@fairwaymc.com>
2021-06-21 15:07:58 -0500

    Update

---
+++ Code/SendGridConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SendGridConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var configs = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", false, true)
+                .Build();
+            var apiKey = configs["API_KEY"];
+        }
+
+        static async Task ExecuteSendEmailAsync(params object[] args)
+        {
+
+        }
+    }
+}
