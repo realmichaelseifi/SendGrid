882979f1607f962db26582e33e3f727295cbcb2f
Michael Seifi <michael.seifi@fairwaymc.com>
2021-06-21 16:49:08 -0500

    add execute send email

--- Code/SendGridConsole/Program.cs
+++ Code/SendGridConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,18 +10,45 @@ namespace SendGridConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string API_KEY;
+        private static string FROM_ADDRESS;
+        public static string[] TO_ADDRESS;
+
+        static async Task  Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             var configs = new ConfigurationBuilder()
                 .AddJsonFile($"appsettings.json", false, true)
                 .Build();
-            var apiKey = configs["API_KEY"];
+            API_KEY = configs["API_KEY"];
+            FROM_ADDRESS = configs["FROM_ADDRESS"];
+            TO_ADDRESS = configs["TO_ADDRESS"].Split(';');
+
+            await ExecuteSendEmailAsync(args).ConfigureAwait(false);
         }
 
-        static async Task ExecuteSendEmailAsync(params object[] args)
+        static async Task ExecuteSendEmailAsync(params string[] args)
         {
+            var client = new SendGridClient(new SendGridClientOptions
+            {
+                ApiKey = API_KEY
+            });
 
+            var from = new EmailAddress(FROM_ADDRESS, "Notification System");
+            var tos = new List<EmailAddress>();
+            foreach (var to in TO_ADDRESS)
+            {
+                tos.Add(new EmailAddress(to));
+            }
+
+            var subject = "Testing SendGrid Notification System";
+            var body = "Testing SendGrid Notification System";
+            var content = "<strong>Reply back to let me know that you got this</strong><br/><br/>Michael Seifi";
+
+            var msg = MailHelper.CreateSingleEmailToMultipleRecipients(
+                from, tos, subject, body, content);
+
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
         }
     }
 }
